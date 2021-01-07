ab304b2e053ff16ecec80b61fb26528b52dab23a
Selçuk Bilgen <selcukbilgen@hotmail.com>
2021-01-07 23:57:17 +0300

    created header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,39 @@
 <template>
-  <header>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/direct">Direct</router-link> |
-    <router-link to="/explore">Explore</router-link> |
-    <router-link to="/profile">Profile</router-link>
+  <header class="header">
+    <router-link to="/">Instagram</router-link>
+
+    <form>
+      <input type="text" placeholder="Search..." />
+    </form>
+
+    <nav>
+      <router-link to="/"> <IconHome /> <IconHomeFill /> </router-link>
+      <router-link to="/direct">Direct</router-link>
+      <router-link to="/explore">Explore</router-link>
+      <router-link to="/profile">Profile</router-link>
+    </nav>
   </header>
 </template>
 
 <script>
+import IconHome from "@/icons/home.svg";
+import IconHomeFill from "@/icons/home-fill.svg";
+
 export default {
   name: "MainHeader",
+  components: {
+    IconHome,
+    IconHomeFill,
+  },
 };
 </script>
+
+<style scoped>
+.header {
+  background-color: yellow;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  align-items: center;
+  padding: 20px;
+}
+</style>
--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home">
     <CustomText tag="h1">Profile Index</CustomText>
-
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque molestias
+      autem distinctio accusantium, velit sint itaque assumenda amet repudiandae
+      at debitis in quidem, ipsam, rem modi fuga odio unde qui.
+    </p>
     <div id="nav">
       <router-link to="/profile">Post</router-link> :)
       <router-link to="/profile/igtv">IGTV</router-link> :')
